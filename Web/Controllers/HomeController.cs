de5e9b2caaccb1539efdbe1dd47e07279840ef1d
Aryo <aryobd@gmail.com>
2024-11-24 13:45:21 +0700

    NATIVE QUERY MENGGUNAKAN PARAMETER.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -28,10 +28,19 @@ a.meter_size_code
 
 from comm.tr_class a
 
+where a.group_code = @group_code --> MENGGUNAKAN PARAMETER
+
 order by a.group_code, a.class_code --> ORDER
 
-offset 20 limit 10 --> PAGING --> MULAI DARI BARIS 21 SEBANYAK 10 BARIS
+--offset 20 limit 10 --> PAGING --> MULAI DARI BARIS 21 SEBANYAK 10 BARIS
 ";
+            //var param = new NpgsqlParameter("@group_code", NpgsqlTypes.NpgsqlDbType.Smallint) { Value = (Int16)1 };
+            //cmd.Parameters.Add(param);
+
+            NpgsqlParameter param1 = new NpgsqlParameter("@group_code", NpgsqlTypes.NpgsqlDbType.Smallint) { Value = (Int16)1 };
+            cmd.Parameters.Add(param1);
+
+            //NpgsqlTransaction trans = conn.BeginTransaction();
 
             try
             {
@@ -44,16 +53,21 @@ offset 20 limit 10 --> PAGING --> MULAI DARI BARIS 21 SEBANYAK 10 BARIS
                     itm.group_code = Convert.ToInt32(sdr["group_code"]);
                     itm.class_code = sdr["class_code"].ToString();
                     itm.class_desc = sdr["class_desc"].ToString();
-                    //if (sdr["meter_size_code"] == null)
-                    //    itm.meter_size_code = null;
-                    //else
-                    //    itm.meter_size_code = Convert.ToInt32(sdr["meter_size_code"]);
+
+                    if (sdr["meter_size_code"] != DBNull.Value)
+                        itm.meter_size_code = Convert.ToInt32(sdr["meter_size_code"]);
+                    else
+                        itm.meter_size_code = null;
 
                     lst.Add(itm);
                 }
+
+                //trans.Commit();
             }
             catch (Exception ex)
             {
+                //trans.Rollback();
+
                 return View(ex.Message);
             }
 
